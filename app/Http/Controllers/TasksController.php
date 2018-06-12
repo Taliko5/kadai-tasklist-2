727b92d6fd0e5218cd4e909c32e10abcf58dcde8
noliko-zwa <noicomimo79@gmail.com>
2018-06-12 17:56:36 +0000

    Procfile

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Task; 
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $data = [];
+        if(\Auth::check()) {
+            $user = \Auth::user();
+            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);  
+            
+            return view('tasks.index', [
+                'tasks' => $tasks,
+            ]);
+        }else {
+           return view('welcome');
+        }
+    }     
+      
+    
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $user = \Auth::user();
+        $task = new Task;
+        return view('tasks.create', [
+            'user' => $user,
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            "status" => 'required',
+            "content" => 'required|max:191',
+            ]);
+        $request->user()->tasks()->create([
+          'content' => $request->content,
+          'status' => $request->status,
+        ]);
+
+        return redirect('/');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+       $task = Task::find($id);
+
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+         $task = Task::find($id);
+   
+     return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+         $this->validate($request,[
+            "status" => 'required',
+            "content" => 'required|max:191',
+            ]);
+       $task = Task::find($id);
+       $task->status =$request->status;
+        $task->content = $request->content;
+        $task->save();
+
+        return redirect('/');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+    $task = \App\Task::find($id);
+    if (\Auth::user()->id === $task->user_id){
+      
+        $task->delete();
+}
+        return redirect()->previous();
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,52 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+       <meta charset="utf-8">
+        <title>Tasklist</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
 
-@section('content')
-<!-- this is a space fpr contents for index -->
-<h1>メッセージ一覧</h1>
-@foreach ($tasks as $task)
-        <table class="table table-bordered table-dark">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>status</th>
-                    <th>text</th>
-                </tr>
-            </thead>
-            <tbody>
-                 <tr><td>{{ $task->user_id }}  </td>
-                <td>  {{ $task->status }}  </td>
-                <td> {{ $task->content }} </span>  </td>
-           </tr>
-            @endforeach
-        </tbody>
-        </table>
-            
-  {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class'=>'btn btn-warning']) !!}
-
-@endsection
+    <body>
+       
+        <header>
+            <!-- ナビ --!>
+            <nav class="navbar navbar-default navbar-static-top">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" deta-target="#bs-example-navbar-collapse-1">
+                      <span class="sr-only">Toggle navigation</span>
+                       <span class="icon-bar"></span>   
+                       <span class="icon-bar"></span> 
+                       <span class="icon-bar"></span>
+                       </button>
+                       <a class="navbar-brand" href="/">Tasklist</a>
+                        </div>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <ul class="nav navbar-nav navbar-right">
+                              @if (Auth::check())
+                              <li> {!! link_to_route('tasks.index', 'Users') !!}</li>
+                              <li class='dropdown'>
+                              <a href="#">my pforile</a> </li>
+                              <li role='separator' class="divider"></li>
+                              <li> {!! link_to_route("tasks.create",'creating a new message') !!} </li>
+                            <li>{!! link_to_route('logout.get', 'logout') !!} </li>
+                             </ul>
+                             @else
+                             <li>{!! link_to_route('signup.get', 'Signup') !!} </li>
+                            <li>{!! link_to_route('login', 'Login') !!}</li>
+                             @endif
+                             </ul>
+                        </div>
+                        </div>
+            </nav>
+        </header>
+         @include('commons.error_tasks')
+        @yield('content')
+        
+    </body>
+</html>
