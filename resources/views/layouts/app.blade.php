fa8a910aeec86e66f23fe9e6479b3708fc17f96e
noliko-zwa <noicomimo79@gmail.com>
2018-06-12 16:37:06 +0000

    top

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- this is a space fpr contents for index -->
+<h1>メッセージ一覧</h1>
+@foreach ($tasks as $task)
+        <table class="table table-bordered table-dark">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>status</th>
+                    <th>text</th>
+                </tr>
+            </thead>
+            <tbody>
+                 <tr><td>{{ $task->user_id }}  </td>
+                <td>  {{ $task->status }}  </td>
+                <td> {{ $task->content }} </span>  </td>
+           </tr>
+            @endforeach
+        </tbody>
+        </table>
+            
+  {!! link_to_route('tasks.create', '新規メッセージの投稿', null, ['class'=>'btn btn-warning']) !!}
+
+@endsection
